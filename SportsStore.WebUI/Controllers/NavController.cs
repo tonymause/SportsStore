84da1782ca1cf56fdfcc799232314b194dff12c3
wei bin <tonymause@163.com>
2016-03-14 11:25:45 +0800

    added mobile web development

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -21,7 +21,7 @@ namespace SportsStore.WebUI.Controllers
                 .Distinct()
                 .OrderBy(x => x);
 
-            return PartialView(categories);
+            return PartialView("FlexMenu", categories);
         }
     }
 }
